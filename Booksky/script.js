ae9299ef6145cb97810bfdd27d291f1b6e6607e7
Aaron <115331438+Aaron2578@users.noreply.github.com>
2024-12-07 11:14:23 +0530

    initial commit

---
+++ Booksky/script.js
@@ -0,0 +1,29 @@
+var add_book = document.getElementById('add_book');
+var box_overlay = document.getElementById('box_overlay');
+var add = document.getElementById('add');
+var cancel = document.getElementById('cancel');
+var container = document.querySelector('.container');
+var book_name = document.getElementById('book_name');
+var author_name = document.getElementById('author_name');
+var description = document.getElementById('description');
+
+add_book.addEventListener("click",function() {
+    box_overlay.style.display='block';
+}) 
+
+add.addEventListener("click", function(event) {
+    event.preventDefault();
+    var div = document.createElement('div');
+    div.setAttribute('class','book-wrapper');
+    div.innerHTML=`<h2>${book_name.value}</h2><h4>${author_name.value}</h4><p>${description.value}</p><button onclick="deletebox(event)">Delete</button>`
+    container.append(div);
+    box_overlay.style.display='none';
+
+})
+cancel.addEventListener('click', function(event) {
+    event.preventDefault();
+    box_overlay.style.display='none';
+})
+function deletebox(event) {
+    event.target.parentElement.remove();
+}
